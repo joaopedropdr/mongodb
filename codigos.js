28b4b29612887ebec62411d6dc1027fcbf47d07c
João Pedro Pascuci De Russi <jpderussi@gmail.com>
2026-04-22 11:59:58 -0300

    Aula 22-04

--- codigos.js
+++ codigos.js
@@ -61,4 +61,97 @@ db.pessoas.aggregate([
     {$out: "proprietarios"}
 ])  
 
+db.veiculos.aggregate([
+    { $sample: { size: 5 } },
+    {
+        $lookup: {
+            from: "proprietarios",
+            localField: "proprietario_id",
+            foreignField: "_id",
+            as: "proprietario_info"
+        }
 
+    },
+    {
+        $project: {
+            _id: 0,
+            marca: 1,
+            modelo: 1,
+            ano: 1,
+            placa: 1,
+            cor: 1,
+            renavan: 1,
+            proprietario_id: "$proprietario_info._id"
+        }
+    },
+])
+
+db.veiculos.aggregate([
+    {
+        $project: {
+            _id: 0,
+            marca: 1
+        }
+    },
+    { $out: "marcas"}
+])
+
+
+db.veiculos.aggregate([
+    {
+        $group: {
+            _id: "$marca", 
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            nome_marca: "$_id",
+        }
+    },
+    {
+        $out: "marcas"
+    }
+
+])
+
+db.veiculos.aggregate([
+    { $limit: 100 }, 
+    {
+        $lookup: {
+            from: "marcas",
+            localField: "marca",       // Campo na coleção veiculos
+            foreignField: "nome_marca", // Campo na coleção marcas
+            as: "info_marca"
+        }
+    },
+
+    {
+        $lookup: {
+            from: "proprietarios",
+            localField: "",       // Campo na coleção veiculos
+            foreignField: "nome_marca", // Campo na coleção marcas
+            as: "info_marca"
+        }
+    },
+    
+    {
+        $unwind: {
+            path: "$info_marca",
+            preserveNullAndEmptyArrays: true // Mantém o veículo se não achar a marca
+        }
+    },
+    {
+        $project: {
+            _id: 1,
+            modelo: 1,
+            ano: 1,
+            placa: 1,
+            marca_veiculo: {
+                nome_marca: "$info_marca.nome_marca",
+                marca_id: "$info_marca._id"
+            }
+           
+        }
+    },
+])
